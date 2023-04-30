3c3e909dafcfa563c20b7c7bc931dcdd8d3429ab
LeKosfrancuz <mateo.kos100@gmail.com>
2023-04-30 16:27:03 +0200

    Napravio upis u file i doradio semafor 	deleted:    Semafor.out 	new file:   build.sh 	deleted:    log.txt 	modified:   semafor.c

---
+++ build.sh
@@ -0,0 +1,4 @@
+gcc semafor.c -o Semafor.out -lm -Wall -Wextra &&
+rm file.txt &&
+rm log.txt
+
--- semafor.c
+++ semafor.c
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <time.h>
 #include <string.h>
+#include <math.h>
+#include <ctype.h>
 
 typedef char* Cstr;
 
@@ -14,23 +16,29 @@ void ProcesTreeCreator();
 void proc1();
 void proc2();
 void proc6();
+Cstr IntToKomKom(int input);
+Cstr StrLow(Cstr input);
 void Error(Cstr message) {
 	printf("\e[1;31mERROR: \e[1;37m%s\e[0;37m\n", message);
 	exit(1);
 }
 
 #define BROJ_PROCESA 8
-#define RANDOM 1
 
-Cstr LOG_PATH = "log.txt";  // "out" za stdout i "err" za stderr 
+int RANDOM = 0;				// Paljenje i gašenje randomizacije virualnih poslova
+Cstr LOG_PATH = "log.txt";		// "out" za stdout i "err" za stderr 
 Cstr FILE_PATH = "file.txt";
-int procPipe[BROJ_PROCESA*2][2]; // 2 stanja (0 za citanje i 1 za pisanje)
+int procPipe[BROJ_PROCESA*2][2];	// 2 stanja (0 za citanje i 1 za pisanje)
+int VRIJEME_PISANJA = 0;		// Vrijeme pisanja u kriticnom odsjecku
 
 enum KomunikacijskeKomande {
 	EXIT = -1, WAIT, READY, GO, DONE
 };
 
 int main() {
+
+
+// ***** POSTAVLJANJE KOMUNIKACIJE S PROCESIMA *****
 	for (int i = 0; i < BROJ_PROCESA*2; i++)
 		if (pipe(procPipe[i]) == -1) Error("Komunikacija preko \"Pipe-a\" nije uspjela");
 
@@ -51,12 +59,12 @@ int main() {
 	}
 	 
 
-	int komanda = GO, count = 0;
-	while (count < 3) {
-		count++;
-		for (int i = 0; i < BROJ_PROCESA; i++) {
+	int komanda = GO;
+	while (komanda != EXIT) {
+		for (int i = 0; i < BROJ_PROCESA && komanda != EXIT; i++) {
 			int dobivenaPoruka;
 			if (read(procPipe[i+BROJ_PROCESA][0], &dobivenaPoruka, 4) == -1) continue;
+			else if (dobivenaPoruka == EXIT) { komanda = EXIT; printf("Id %d poslao zahtjev o gašenju programa\n", i); continue; }
 			else if (dobivenaPoruka != READY) continue;
 			
 			komanda = GO;
@@ -65,6 +73,7 @@ int main() {
 			do {
 				read(procPipe[i+BROJ_PROCESA][0], &dobivenaPoruka, 4);
 			} while (dobivenaPoruka != DONE);
+			printf("Id %d pisao u file!\n", i);
 		}
 	}
 
@@ -87,50 +96,73 @@ int KillRunning() {
 	int kill = EXIT;
 	for (int i = 0; i < BROJ_PROCESA; i++) {
 		write(procPipe[i][1], &kill, 4);
-		fprintf(log, "Poslan KILL(%d) procesu #%d\n", EXIT, i);
+		fprintf(log, "Poslan KILL procesu #%d\n", i);
 	}
-
+	
+	int returnVal = 0;
 	for (int i = BROJ_PROCESA; i < BROJ_PROCESA*2; i++) {
-		int retry = 9;
+		const int maxRetry = 10;
+		int retry = maxRetry;
 		do {
 			read(procPipe[i][0], &kill, 4);
-			fprintf(log, "Primljen %d od procesa #%d (preostali broj pokusaja %d)\n", kill, i, retry);
-			retry--;
-			sleep(0.1);  // Da proces ima vremena odgovoriti ukoliko zatvara FILE ili slicno
+			fprintf(log, "Primljen %5s od procesa #%d (preostali broj pokusaja %d)\n", IntToKomKom(kill), i-BROJ_PROCESA, retry);
+			if (kill != EXIT) retry--;
+			sleep(0.01 * pow(2, maxRetry-retry));  // Da proces ima vremena odgovoriti ukoliko zatvara FILE ili slicno
 		} while (kill != EXIT && retry > 0);
-		if (retry <= 0) return -1;
+		if (retry <= 0) { fprintf(log, "Proces #%d nije odgovorio na zahtjev KILL\n", i-BROJ_PROCESA); returnVal = -1; }
 	}
 
 	fclose(log);
-	return 0;
+	return returnVal;
 }
 
-void WriteToFile(int id, int vrijemeCekanjaMs) {
+void WriteToFile(int id, int vrijemeIzmedjuPisanjaMs) {
 	close(procPipe[id+BROJ_PROCESA][0]);  // Zabrana citanja s id*2 pipe
 	close(procPipe[id][1]);   //  Zabrana pisanja na id pipe
 	int poruka = WAIT;
 	srand(id*time(NULL));
+
+	int count = 0;
+
 	while(poruka != EXIT) {
-		float pravoVrijemeCekanja = vrijemeCekanjaMs/1000.0 + (float)rand()/(float)RAND_MAX*1.96*RANDOM;
-		sleep(pravoVrijemeCekanja);
+		float pravoVrijemeIzmedjuPisanja = vrijemeIzmedjuPisanjaMs/1000.0 + (float)rand()/(float)RAND_MAX*1.96*RANDOM;
+										 // Daje random broj od 0 - 1.96       ^Omogućuje randomizaciju
+		sleep(pravoVrijemeIzmedjuPisanja);
 
 		poruka = READY;
 		write(procPipe[id+BROJ_PROCESA][1], &poruka, 4);
+		
+		clock_t pocetakCekanja, krajCekanja; 
+		pocetakCekanja = clock();
 
 		while (poruka != EXIT && poruka != WAIT){
 			do {
 				read(procPipe[id][0], &poruka, 4); // Citanje trenutne komande
 			} while (poruka == WAIT && poruka != GO && poruka != EXIT);
 			if (poruka == EXIT) continue;
+			
+			krajCekanja = clock();
 
-			printf("Ovo je proces #%d (%d) i ceka %1.2fs  (+%fs)\n", id, getpid(), vrijemeCekanjaMs/1000.0, 
-									pravoVrijemeCekanja - vrijemeCekanjaMs/1000.0);
-			//Error("WRITE to FILE not implemented");
+			FILE *fp;
+			fp = fopen(FILE_PATH, "a");
+			count++;
+
+			sleep(0); // Emulacija programa koji dugo piše unutar kritičnog odsječka
+
+			float vrijemeCekanjaMs = (krajCekanja - pocetakCekanja / (float)CLOCKS_PER_SEC);
+			fprintf(fp, "Ovo je proces #%d (%d) i cekao je %4.4fms", id, getpid(), vrijemeCekanjaMs);
+			fprintf(fp, " | Vrijeme između pisanja:  %1.2fs", vrijemeIzmedjuPisanjaMs/1000.0);
+			if (RANDOM) fprintf(fp, " (+%fs)", pravoVrijemeIzmedjuPisanja - vrijemeIzmedjuPisanjaMs/1000.0); 
+			fprintf(fp, "\n");
+
+			fclose(fp);
 
 			poruka = DONE;
 			write(procPipe[id+BROJ_PROCESA][1], &poruka, 4);
 			poruka = WAIT;
 		}
+
+		if (id == 0 && count >= 3) { poruka = EXIT; write(procPipe[id+BROJ_PROCESA][1], &poruka, 4); poruka = WAIT; }
 	}
 
 	poruka = EXIT; //Potvrda o zavrsetku
@@ -198,3 +230,32 @@ void proc6() {
 	WriteToFile(6, 1000);
 }
 
+Cstr IntToKomKom(int input) {
+	switch (input) {
+		case EXIT: return "EXIT";
+		case WAIT: return "WAIT";
+		case READY: return "READY";
+		case GO: return "GO";
+		case DONE: return "DONE";
+		default: Cstr errout = malloc(50);
+			 sprintf(errout, "Nepoznat Komunikacijski Kod %d", input);
+			 Error(errout);
+	}
+
+	Error("Unreachable");
+	return "";
+}
+
+Cstr ToLow(Cstr input) {
+	
+	int inLen = strlen(input);
+	Cstr output = malloc(inLen + 1);
+	
+	int i;
+	for (i = 0; i < inLen && input[i] != '\0'; i++) 
+		output[i] = tolower(input[i]);
+	for (; i < inLen; i++) 
+		output[i] = '\0';
+
+	return output;
+}
