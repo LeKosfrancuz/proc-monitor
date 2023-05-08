e8c7643de1f3de99b44318b30e8e66a078a5fd1b
LeKosfrancuz <mateo.kos100@gmail.com>
2023-05-08 23:31:25 +0200

    Pametnija analiza CLI argumenata Korišten lev.c za analizu svakod neprepoznatog argumenta i automatska zamjena sa pravim ukoliko je program siguran u to (inace pokaze opcije) 	modified:   src/CLIProcesor.c
     Date:      Mon May 8 23:31:25 2023 +0200

--- src/CLIProcesor.c
+++ src/CLIProcesor.c
@@ -2,58 +2,66 @@
 #define CLI_PROC_C
 
 #include "constLib.h"
+#include "lev.c"	// Izracun slicnosti rijeci
 #include <stdio.h>
 
 
 void ApplyCommandLineArgs(int argc, Cstr* argv, Cstr* logPath, Cstr* filePath) {
-
+	Cstr currentArg = 0;
 	for (int i = 1; i < argc; i++) {
 		char argName[250];
-		if (!strcmp(StrLow(argv[i]), "-help")) {
+		if (strcmp(argName, "argReSet")) {
+			const int lenArgi = strlen(argv[i]);
+			currentArg = realloc(currentArg, lenArgi + 1);
+			strncpy(currentArg, argv[i], lenArgi + 1);
+			strcpy(argName, "argSet");
+		}
+
+		if (!strcmp(StrLow(currentArg), "-help")) {
 			printStdCVersion();
 
 			Error("Help not implemented");
 		}
-		else if (!strcmp(StrLow(argv[i]), "-r") || !strcmp(StrLow(argv[i]), "--random")) {
+		else if (!strcmp(StrLow(currentArg), "-r") || !strcmp(StrLow(currentArg), "--random")) {
 			if (i + 1 == argc) {
-				snprintf(argName, 150, "Opcija %s koristi se kao \"%s %s [on/off]\"", argv[i], argv[0], argv[i]);
+				snprintf(argName, 150, "Opcija %s koristi se kao \"%s %s [on/off]\"", currentArg, argv[0], currentArg);
 				Error(argName);
 			}
 			RANDOM = (!strcmp(StrLow(argv[++i]), "on")) ? 1 : 0;
 		}
-		else if (!strcmp(StrLow(argv[i]), "-c") || !strcmp(StrLow(argv[i]), "--count")) {
+		else if (!strcmp(StrLow(currentArg), "-c") || !strcmp(StrLow(currentArg), "--count")) {
 			if (i + 1 == argc) {
-				snprintf(argName, 150, "Opcija %s koristi se kao \"%s %s [broj pisanja procesa #0]\"", argv[i], argv[0], argv[i]);
+				snprintf(argName, 150, "Opcija %s koristi se kao \"%s %s [broj pisanja procesa #0]\"", currentArg, argv[0], currentArg);
 				Error(argName);
 			}
 			BROJ_PISANJA_PROCESA0 = atoi(argv[++i]);
 		}
-		else if (!strcmp(StrLow(argv[i]), "-t") || !strcmp(StrLow(argv[i]), "--time-of-writing")) {
+		else if (!strcmp(StrLow(currentArg), "-t") || !strcmp(StrLow(currentArg), "--time-of-writing")) {
 			if (i + 1 == argc) {
-				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [vrijeme pisanja svakog procesa]\"", argv[i], argv[0], argv[i]);
+				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [vrijeme pisanja svakog procesa]\"", currentArg, argv[0], currentArg);
 				Error(argName);
 			}
 		 	VRIJEME_PISANJA = atoi(argv[++i]);
 		}
-		else if (!strcmp(StrLow(argv[i]), "-log") || !strcmp(StrLow(argv[i]), "--log-path")) {
+		else if (!strcmp(StrLow(currentArg), "-log") || !strcmp(StrLow(currentArg), "--log-path")) {
 			if (i + 1 == argc) {
-				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [putanja do lokacije za spremanje log datoteke]\"", argv[i], argv[0], argv[i]);
+				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [putanja do lokacije za spremanje log datoteke]\"", currentArg, argv[0], currentArg);
 				Error(argName);
 			}
 			*logPath = realloc(*logPath, strlen(argv[++i]) + 1);
 			strcpy(*logPath, argv[i]);
 		}
-		else if (!strcmp(StrLow(argv[i]), "-o") || !strcmp(StrLow(argv[i]), "--file-path")) {
+		else if (!strcmp(StrLow(currentArg), "-o") || !strcmp(StrLow(currentArg), "--file-path")) {
 			if (i + 1 == argc) {
-				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [putanja do lokacije za spremanje datoteke]\"", argv[i], argv[0], argv[i]);
+				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [putanja do lokacije za spremanje datoteke]\"", currentArg, argv[0], currentArg);
 				Error(argName);
 			}
 			*filePath = realloc(*filePath, strlen(argv[++i]) + 1);
 			strcpy(*filePath, argv[i]);
 		}
-		else if (!strcmp(StrLow(argv[i]), "-tbw") || !strcmp(StrLow(argv[i]), "--time-before-write")) {
+		else if (!strcmp(StrLow(currentArg), "-tbw") || !strcmp(StrLow(currentArg), "--time-before-write")) {
 			if (i + 2 >= argc) {
-				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [#procesa/all] [vrijeme u ms]\"", argv[i], argv[0], argv[i]);
+				snprintf(argName, 250, "Opcija %s koristi se kao \"%s %s [#procesa/all] [vrijeme u ms]\"", currentArg, argv[0], currentArg);
 				Error(argName);
 			}
 			if (!strcmp(StrLow(argv[++i]), "all")) {
@@ -67,10 +75,26 @@ void ApplyCommandLineArgs(int argc, Cstr* argv, Cstr* logPath, Cstr* filePath) {
 			}
 		}
 		else if (argc != 1) {
-			snprintf(argName, 150, "Argument (%s) nije prepoznat\n\x1B[0;37mUpiši \"%s -help\" za pomoć!", argv[i], argv[0]);
-			Error(argName);
+			#define BROJ_KOMANDI 14
+			Cstr komande[BROJ_KOMANDI] = {"-h", "--help", "-r", "--random", "-c", "--count", "-t", "--time-of-writing", "-log", "--log-path", "-o", "--file-path", "-tbw", "--time-before-write"};
+			int index = indexNajSlicnijeRijeci(3, currentArg, komande, BROJ_KOMANDI);
+
+			if (index == -1) {
+				snprintf(argName, 150, "Argument (%s) nije prepoznat\n\x1B[0;37mUpiši \"%s -help\" za pomoć!", currentArg, argv[0]);
+				Error(argName);
+			} else {
+				const int duljinaKomande = strlen(komande[index]);
+				currentArg = (Cstr)realloc(currentArg, duljinaKomande + 1);
+				strncpy(currentArg, komande[index], duljinaKomande + 1);
+				strcpy(argName, "argReSet");
+				--i;
+			}
+
+			
+			#undef BROJ_KOMANDI	
 		}
 	}
+	free(currentArg);
 }
 
 
